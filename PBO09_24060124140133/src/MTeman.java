38063051d066c0ebc6c9060e4b243d0cee3b196f
Muhammad Hafidh Al-Ghifari <alghifarihafidh@gmail.com>
2026-05-10 22:14:05 +0700

    Pertemuan 9 Selesai

--- PBO09_24060124140133/src/MTeman.java
+++ PBO09_24060124140133/src/MTeman.java
@@ -8,16 +8,13 @@ public class MTeman {
     public static void main(String[] args) {
 
         Teman t = new Teman();
-
         t.addNama("Budi");
         t.addNama("Ani");
         t.addNama("Citra");
         t.addNama("Budi"); 
 
         t.showTeman();
-
         System.out.println("Jumlah teman: " + t.getNbelm()); 
-
         System.out.println("Indeks ke-1: " + t.getNama(1)); 
 
         t.setNama("Dewi", 1);
@@ -29,9 +26,7 @@ public class MTeman {
         t.gantiNama("Citra", "Clara");
         t.showTeman();
 
-        // h. countNama
         System.out.println("Budi muncul: " + t.countNama("Budi") + "x"); 
-
         t.delNama("Budi"); 
         t.showTeman();
     }
